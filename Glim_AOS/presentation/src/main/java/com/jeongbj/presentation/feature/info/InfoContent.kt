679bddc83fa359be368d14acf77db18ffe37e212
JeongBJ <ebjdev@gmail.com>
2026-07-02 10:10:31 +0900

    [AOS] feat : Info 가로화면 추가

--- Glim_AOS/presentation/src/main/java/com/jeongbj/presentation/feature/info/InfoContent.kt
+++ Glim_AOS/presentation/src/main/java/com/jeongbj/presentation/feature/info/InfoContent.kt
@@ -1,5 +1,9 @@
 package com.jeongbj.presentation.feature.info
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -15,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -34,14 +39,141 @@ fun InfoContent(
     state: InfoState,
     onAction: (InfoAction) -> Unit,
     modifier: Modifier = Modifier,
-    quotes: LazyPagingItems<QuoteThumbnail>
+    quotes: LazyPagingItems<QuoteThumbnail>,
 ) {
-    LazyVerticalGrid (
+    BoxWithConstraints(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF0A0A1A))
+    ) {
+        if (maxWidth < 600.dp) {
+            InfoPortrait(
+                state = state,
+                onAction = onAction,
+                modifier = modifier,
+                quotes = quotes
+            )
+        } else {
+            InfoLandScape(
+                state = state,
+                onAction = onAction,
+                modifier = modifier,
+                quotes = quotes
+            )
+        }
+    }
+}
+
+@Composable
+fun InfoLandScape(
+    state: InfoState,
+    onAction: (InfoAction) -> Unit,
+    modifier: Modifier = Modifier,
+    quotes: LazyPagingItems<QuoteThumbnail>,
+) {
+    Row(
+        modifier = modifier.fillMaxSize(),
+
+        ) {
+
+        Column(
+            modifier = modifier.weight(1f)
+                .padding(horizontal = 16.dp)
+        ) {
+            InfoHeaderSection(
+                modifier = modifier,
+                user = state.userInfo?.user,
+                onProfileImageClicked = { onAction(InfoAction.OnProfileImageClicked) },
+                onSettingClicked = { onAction(InfoAction.OnSettingClicked) },
+                isOwner = state.isOwner
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            GlimGrassSection(
+                modifier = modifier.padding(horizontal = 8.dp),
+                contributions = state.contributions
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
+        LazyVerticalGrid(
+            modifier = modifier.weight(1f),
+            columns = GridCells.Fixed(3)
+        ) {
+            item(
+                span = { GridItemSpan(maxLineSpan) }
+            ) {
+                SecondaryTabRow(
+                    selectedTabIndex = state.selectedTab.ordinal
+                ) {
+                    GlimType.entries.forEach { tab ->
+                        val text = when (tab) {
+                            GlimType.OWN -> state.userInfo?.numQuotes ?: 0
+                            GlimType.LIKED -> state.userInfo?.numLikes ?: 0
+                        }
+                        Tab(
+                            selected = state.selectedTab == tab,
+                            onClick = { onAction(InfoAction.OnTabSelected(tab)) },
+                            icon = {
+                                Icon(
+                                    painter = painterResource(tab.resId),
+                                    contentDescription = null
+                                )
+                            },
+                            text = {
+                                Text(
+                                    text = text.toString(),
+                                    style = MaterialTheme.typography.bodySmall
+                                )
+                            }
+
+                        )
+                    }
+                }
+            }
+            if (quotes.itemCount == 0) {
+                val text = when (state.selectedTab) {
+                    GlimType.OWN -> "나의 감성을 공유해보세요"
+                    GlimType.LIKED -> "좋아요 한 글림이 없어요"
+                }
+
+                item(
+                    span = { GridItemSpan(maxLineSpan) }
+                ) {
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = modifier.padding(vertical = 40.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                return@LazyVerticalGrid
+            }
+            items(quotes.itemCount) { index ->
+                ThumbnailItem(
+                    quote = quotes[index] ?: return@items,
+                    onClicked = { onAction(InfoAction.OnQuoteThumbnailClicked(it)) },
+                    modifier = Modifier.padding(2.dp)
+                )
+            }
+        }
+
+
+
+    }
+}
+
+@Composable
+fun InfoPortrait(
+    state: InfoState,
+    onAction: (InfoAction) -> Unit,
+    modifier: Modifier = Modifier,
+    quotes: LazyPagingItems<QuoteThumbnail>,
+) {
+    LazyVerticalGrid(
         modifier = modifier.fillMaxSize(),
         columns = GridCells.Fixed(3)
     ) {
-
-
         item(
             span = { GridItemSpan(maxLineSpan) }
         ) {
@@ -55,7 +187,7 @@ fun InfoContent(
             Spacer(modifier = Modifier.height(8.dp))
         }
 
-        item (
+        item(
             span = { GridItemSpan(maxLineSpan) }
         ) {
             GlimGrassSection(
@@ -65,7 +197,7 @@ fun InfoContent(
             Spacer(modifier = Modifier.height(8.dp))
         }
 
-        item (
+        item(
             span = { GridItemSpan(maxLineSpan) }
         ) {
             SecondaryTabRow(
@@ -79,21 +211,31 @@ fun InfoContent(
                     Tab(
                         selected = state.selectedTab == tab,
                         onClick = { onAction(InfoAction.OnTabSelected(tab)) },
-                        icon = { Icon(painter = painterResource(tab.resId), contentDescription = null) },
-                        text = { Text(text = text.toString(), style = MaterialTheme.typography.bodySmall) }
+                        icon = {
+                            Icon(
+                                painter = painterResource(tab.resId),
+                                contentDescription = null
+                            )
+                        },
+                        text = {
+                            Text(
+                                text = text.toString(),
+                                style = MaterialTheme.typography.bodySmall
+                            )
+                        }
 
                     )
                 }
             }
         }
 
-        if(quotes.itemCount == 0) {
+        if (quotes.itemCount == 0) {
             val text = when (state.selectedTab) {
                 GlimType.OWN -> "나의 감성을 공유해보세요"
                 GlimType.LIKED -> "좋아요 한 글림이 없어요"
             }
 
-            item (
+            item(
                 span = { GridItemSpan(maxLineSpan) }
             ) {
                 Text(
@@ -109,7 +251,7 @@ fun InfoContent(
 
         items(quotes.itemCount) { index ->
             ThumbnailItem(
-                quote = quotes[index]?: return@items,
+                quote = quotes[index] ?: return@items,
                 onClicked = { onAction(InfoAction.OnQuoteThumbnailClicked(it)) },
                 modifier = Modifier.padding(2.dp)
             )
@@ -123,7 +265,7 @@ fun InfoContentPreview() {
     GlimTheme {
         InfoContent(
             state = InfoState(),
-            onAction = {   },
+            onAction = { },
             quotes = remember {
                 flowOf(
                     PagingData.from(

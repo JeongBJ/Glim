e1ffbfbc02aedf2c1fbc95ce203c942131d9e4a0
JeongBJ <ebjdev@gmail.com>
2026-07-11 05:29:26 +0900

    [BE] feat : FCM 토큰 만료 시 삭제

--- Glim_BackEnd/Glim_Server/src/main/kotlin/com/jeongbj/glim/external/firebase/service/PushService.kt
+++ Glim_BackEnd/Glim_Server/src/main/kotlin/com/jeongbj/glim/external/firebase/service/PushService.kt
@@ -3,21 +3,29 @@ package com.jeongbj.glim.external.firebase.service
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import com.jeongbj.glim.quote.entity.Quote
+import com.jeongbj.glim.user.repository.FcmTokenRepository
 import org.springframework.stereotype.Service
 
 @Service
-class PushService {
+class PushService(
+    private val fcmTokenRepository: FcmTokenRepository
+) {
 
     fun notifyQuoteLiked(fromUserSeq: Long, quote: Quote) {
         if (fromUserSeq == quote.user.userSeq) return
+        println(quote.user.fcmTokens)
 
         quote.user.fcmTokens
             .filter { it.pushEnabled }
-            .forEach {
-                sendLikeNotification(
-                    token = it.token,
-                    quoteSeq = quote.quoteSeq
-                )
+            .forEach { fcmToken ->
+                runCatching {
+                    sendLikeNotification(
+                        token = fcmToken.token,
+                        quoteSeq = quote.quoteSeq
+                    )
+                }.onFailure {
+                    fcmTokenRepository.delete(fcmToken)
+                }
             }
     }
 
@@ -25,7 +33,6 @@ class PushService {
         token: String,
         quoteSeq: Long
     ) {
-        println(token)
         val message = Message.builder()
             .setToken(token)
             .putData("type", "LIKE")

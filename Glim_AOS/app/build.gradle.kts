a855d8df71ce1258e579cc56fd707f20685333dd
JeongBJ <ebjdev@gmail.com>
2026-07-27 15:11:07 +0900

    [AOS] release : 잠금화면에서 글림 보기, AGP 9.3.1

--- Glim_AOS/app/build.gradle.kts
+++ Glim_AOS/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.jeongbj.glim"
         minSdk = 26
         targetSdk = 37
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
